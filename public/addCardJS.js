710e6eaff9a3e5af7f4540955fb7e3f382b38e63
LeighBarnett <leighbarnett5@gmail.com>
2017-07-03 15:51:08 +0300

    cleaned up files

--- public/addCardJS.js
+++
@@ -1,9 +0,0 @@
-$('.addCardButton').click(function() {
-    console.log("here")
-    var front = $(this).siblings('.frontText').val();
-    var back = $(this).siblings('.backText').val();
-    console.log(front)
-    console.log(back)
-    // var deckId = $(this).closest('deck-containor').data().id;
-    app.addCard(front, back);
-})
